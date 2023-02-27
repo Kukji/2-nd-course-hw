367a74dc3ef851b97d427912b44d21d2e8a33d66
Kukji <ank73821@gmail.com>
2023-02-27 21:46:00 +0500

    4 Домашнее Задание

--- script.js
+++ script.js
@@ -1,77 +1,56 @@
-//1(2Домашка)
-let password = 'пароль'
+//1
+let i = 1;
 
-let checkpassword = prompt('Введите пароль')
+while (i <= 2) {
+    console.log('Привет!');
 
-if (password === checkpassword) {
-    console.log('Пароль введен верно');
-} else {
-    console.log('Пароль введен неправильно');
+    i++;
 }
 
-//2(2Домашка)
-let c = Number(2)
+//2
 
-if (c > 0 && c < 10) {
-    console.log('Верно')
-} else {
-    console.log('Неверно')
+i = 1;
+
+while (i <= 25) {
+    console.log(i);
+
+    i++;
 }
 
-//3(2Домашка)
-let d = Number(10)
-let e = Number(11)
+//3
 
-if (d > 100 || e > 100) {
-    console.log('Верно')
-} else {
-    console.log('Неверно')
+i = 7;
+
+while (i <= 22) {
+    console.log(i);
+
+    i++;
 }
 
-//4(2Домашка)
-let a = Number(2)
-let b = Number(3)
-console.log(a + b)
-//5(2Домашка)
-let monthNumber = String(prompt('Введите число месяца'))
+//4
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400'
+};
 
-switch (monthNumber) {
-    case '1':
-        console.log('Январь');
-        break;
-    case '2':
-        console.log('Февраль');
-        break;
-    case '3':
-        console.log('Март');
-        break;
-    case '4':
-        console.log('Апрель');
-        break;
-    case '5':
-        console.log('Май');
-        break;
-    case '6':
-        console.log('Июнь');
-        break;
-    case '7':
-        console.log('Июль');
-        break;
-    case '8':
-        console.log('Август');
-        break;
-    case '9':
-        console.log('Сентябрь');
-    case '10':
-        console.log('Октябрь');
-        break;
-    case '11':
-        console.log('Ноябрь');
-        break;
-    case '12':
-        console.log('Декабрь');
-        break;
-    default:
-        console.log('Таких месяцев не существует');
-        break;
+for (let key in obj) {
+
+    console.log(`${key} — зарплата ${obj[key]} долларов`);
+
 }
+
+//5
+
+let num = 0;
+for (let n = 1000; n > 50; n /= 2, num++) {
+    console.log(num, n);
+}
+
+//6
+
+for (let Report = 7; Report <= 31; Report += 7) {
+    console.log(`Сегодня пятница, ${Report}-е число. Необходимо подготовить отчет.`);
+}
+
+
