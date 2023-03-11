cae4dd7b9f0a613345683e6993970a6dad9be00f
Kukji <ank73821@gmail.com>
2023-03-11 17:05:34 +0500

    7 dz

--- script.js
+++ script.js
@@ -18,71 +18,113 @@ function GameMonth(params) {
         alert('Досвидания!')
     }
 }
-//1
 
-//2
-const massiv = [1, 5, 4, 10, 0, 3];
+function GameWords() {
+    let arr = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    arr = arr.sort(() => Math.random() - 0.5);
+    alert(arr)
+    let name = String(prompt('Чему равнялся первый элемент массива?'))
+    name = name.toLowerCase()
+    let name2 = String(prompt('Чему равнялся последний элемент массива?'))
+    name2 = name.toLowerCase()
+    if (name === arr[0] && name2 === arr[6]) {
+        alert('Молодец, вы угадали запомнили все слова!')
+        return GameStop()
+    } else if (name === arr[0]) {
+        alert('Почти верно, можно и лучше!')
+        return GameStop()
+    } else if (name2 === arr[6]) {
+        alert('Почти верно, можно и лучше!')
+        return GameStop()
+    } else {
+        alert('Неправильно')
+        return GameStop()
+    }
 
-console.log("Позиция четвёрки", massiv.indexOf(4));
+}
 
-//3
-const arrone = [1, 3, 5, 10, 20];
-let reuslt = arrone.join(' ');
-console.log(reuslt)
-
-//4
-let arr = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
-for (let i = 0; i < 4; i++) {
-    arr[i] = [];
-    for (let j = 0; j <= i; j++) {
-        arr[i][j] = j + 1;
+function GameStop() {
+    let a = confirm('Хочешь продолжить игру?')
+    if (a === true) {
+        return (GameWords())
+    }
+    else {
+        alert('Хорошего дня, уже жду твоего возвращения!')
     }
 }
-console.log(arr);
+
+//1
+let str = 'js'
+console.log(str.toUpperCase())
+
+//2
+let resArr = [];
+function searchStrart(arr, str) {
+    arr.forEach(element => {
+        if (element.toLowerCase().startsWith(str.toLowerCase())) {
+            resArr.push(element)
+        }
+    })
+    console.log(resArr)
+}
+searchStrart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко')
+
+
+//3
+let x = Number(32.58884)
+console.log(Math.floor(x))
+console.log(Math.ceil(x))
+console.log(Math.round(x))
+
+//4
+var myArray = [52, 53, 49, 77, 21, 32];
+var min = Math.min(...myArray);
+max = Math.max(...myArray);
+console.log(min)
+console.log(max)
+
 
 //5
-
-const mas = [1, 1, 1];
-
-let rez = mas.push('2', '2', '2')
-console.log(rez)
-
-//6
-const six = [9, 8, 7, 'a', 6, 6];
-delete six[3];
-let reza = six.sort()
-console.log(reza)
-
-//7
-const UserMassiv = [9, 8, 7, 6, 5];
-let search = UserMassiv.includes(Number(prompt('Введите число от 1 до 10')));
-if (search === true) {
-    console.log('Введенное число содержится в данном массиве');
-} else {
-    console.log('Введенное число не содержится в данном массиве');
+function RandomNumber() {
+    return Math.round(Math.random() * 10)
 }
 
+console.log(RandomNumber())
+//6
+function getRandomArrNumber(getNumber) {
+    const GetArr = []
+    let GetNumberDelTwo
+        = Math.floor(getNumber / 2)
+    for (let i = 0; i < GetNumberDelTwo; i++) {
+        GetArr.push(Math.round(Math.random() * getNumber));
+    }
+    console.log(GetArr)
+}
+getRandomArrNumber(Number(prompt('Введите любое число')))
 
+//7
+let seventhRandomValue = (maxValue, minValue) => {
+    let seventhRandomResult = Math.round(Math.random() * (maxValue - minValue) + minValue);
+    console.log(seventhRandomResult);
+}
+seventhRandomValue(Number(prompt('Введите любое число')), Number(prompt('Введите любое число, значение которого меньше, чем у первого числа')));
 //8
-
-var str = 'fedcba';
-var strReverse = str.split('').reverse().join('');
-console.log(strReverse)
+let currentDate = new Date();
+console.log(currentDate);
 
 //9
 
-const FirstNum = [1, 2, 3]
-const SeckondNum = [4, 5, 6]
-const rezult = [...FirstNum, ...SeckondNum]
-alert(rezult);
+let CurrenDate = new Date("10 March 2023")
+CurrenDate.setDate(CurrenDate.getDate() + 73);
+console.log(CurrenDate);
 
 //10
 
-const RandomNumber = [6, 5, 8, 7, 9];
-
-let i = 0
-
-while (RandomNumber[i + 1] != undefined) {
-    console.log(RandomNumber[i] + RandomNumber[i + 1]);
-    i++;
+let dating = new Date();
+function Data(dating) {
+    const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    const moths = ["Января", "Февраля", "Марта", "Ареля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+    let date = dating.getDate() + ' ' + moths[dating.getMonth()] + ' ' + dating.toTimeString();
+    console.log(date)
 }
+Data(dating)
